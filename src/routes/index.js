13b75d829f28559b97888e5cd8b371499f21fd96
NguyenHuynhThanhDuy <93082326+NguyenHuynhThanhDuy@users.noreply.github.com>
2022-10-24 09:24:18 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,24 +2,12 @@ import Home from '~/pages/Home';
 import Login from '~/pages/auth/login';
 import Signup from '~/pages/auth/signup';
 import Forgotpassword from '~/pages/auth/forgotaccount/forgotPassword';
-import rqotb from '~/pages/auth/forgotaccount/rqOtb';
-import changePassword from '~/pages/auth/forgotaccount/changePassowrd';
-import loginAdmin from '~/admin/Login';
-import homeAdmin from '~/admin/Home';
-import userAdmin from '~/admin/UserAdmin';
-import { LayoutLogin } from '~/components/Layout';
+
 
 
 const publicRoutes = [
     { path: '/', component: Home },
-    { path: '/login', component: Login, layout: LayoutLogin },
-    { path: '/signup', component: Signup, layout: LayoutLogin },
-    { path: '/forgotpassword', component: Forgotpassword, layout: LayoutLogin },
-    { path: '/request-OTP', component: rqotb, layout: LayoutLogin },
-    { path: '/changePassword', component: changePassword, layout: LayoutLogin },
-    { path: '/LoginAdmin', component: loginAdmin, layout: null },
-    { path: '/HomeAdmin', component: homeAdmin, layout: null },
-    { path: '/UserAdmin', component: userAdmin, layout: null },
+
 ]
 
 const privateRoutes = [
