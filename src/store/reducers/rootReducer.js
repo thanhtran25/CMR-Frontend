a201ce6a0b4735f4fa7c21d40188bf93285f6a84
NguyenHuynhThanhDuy <duy56vn@gmail.com>
2022-11-22 19:12:55 +0700

    [Modified]: UI

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import userReducer from "./userReducer";
 import productReducer from "./productReducer";
 import cartReducer from "./cartReducer";
+import adminReducer from "./adminReducer";
 
 const mainReducer = combineReducers({
     user: userReducer,
     product: productReducer,
     cart: cartReducer,
+    admin: adminReducer,
 })
 
 export default mainReducer
